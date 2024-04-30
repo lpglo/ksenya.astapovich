870213af0e559691a011f216373ef7778e5c3231
KsenyaAst <astapovichksenya@gmail.com>
2024-04-30 13:18:08 +0300

    homework_3 done

---
+++ homework/Homework_3/task_2.py
@@ -0,0 +1,9 @@
+
+a = 7
+b = 5
+
+difference_result = a - b
+print("Difference of a и b equals to:", difference_result)
+
+multiplication_result = a * b
+print("multiplication of a и b equals to:", multiplication_result)
---
+++ homework/Homework_3/task_4.py
@@ -0,0 +1,10 @@
+import math
+
+a = 7
+b = 5
+
+hypotenuse = math.sqrt(a**2 + b**2)
+print("The hypotenuse of a triangle is equal to:", hypotenuse)
+
+area = 0.5 * a * b
+print("The area of the triangle is:", area)
