c2d83e879d3dfc8fbe641fd3404ebfd40f471332
Kseniya <astapovichksenya@gmail.com>
2024-12-12 11:08:07 +0300

    updated <что был line

--- homework/Homework_9/task_2.py
+++ homework/Homework_9/task_2.py
@@ -1,3 +1,4 @@
+
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
 
 hot_days = list(filter(lambda temp: temp > 28, temperatures))
